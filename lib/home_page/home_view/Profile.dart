1f468b2c8670e5b506ea43f5b126ed3daa9442cd
Hai Ton That <haitt.bkdn@gmail.com>
2023-11-21 00:42:34 +0700

    sang 21_11

--- lib/home_page/home_view/Profile.dart
+++ lib/home_page/home_view/Profile.dart
@@ -5,7 +5,22 @@ import 'package:shared_preferences/shared_preferences.dart';
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ProfileState();
+    return Container(
+      color: Colors.purple[50], // Màu tím nhạt
+      child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            ProfileState(),
+            Positioned(
+              top: 70,
+              child: CircleAvatar(
+                radius: 50, // Đường kính của ảnh đại diện
+                backgroundImage: AssetImage('assets/images/anh3.jpg'), // Đường dẫn đến ảnh đại diện
+              ),
+            ),
+          ]
+      ),
+    );
   }
 }
 
@@ -77,33 +92,143 @@ class _ProfileState extends State<ProfileState> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.purple[100],
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Email: $email'),
-            Text('Phone Number: $phoneNumber'),
-            Text('Username: $username'),
-            Text('Address: $address'),
-            ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return ChangePasswordModal();
-                  },
-                );
-              },
-              child: Text('Change Password'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _showLogoutConfirmation(context);
-              },
-              child: Text('Logout'),
-            ),
-          ],
+        child: Container(
+          height: 376.0,
+          width: 300.0, // Thêm chấm động sau số để chỉ ra đây là kiểu double
+          padding: EdgeInsets.all(41.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+            color: Colors.white,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center, // Sửa thành CrossAxisAlignment.center để canh giữa
+            children: <Widget>[
+              Center(
+                child: Column(
+                  children: [
+                    Text(
+                      '$username',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25.0, // Thêm chấm động sau số để chỉ ra đây là kiểu double
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text('Đã upload 120 files'),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 41,
+                width: 200,
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2), // Thêm padding bên trong Container
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                    Text('Email:', style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text('$email'),
+                  ]
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 41,
+                width: 200,
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2), // Thêm padding bên trong Container
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Số điện thoại:', style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text('$phoneNumber'),
+                    ]
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 41,
+                width: 200,
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2), // Thêm padding bên trong Container
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Địa chỉ: ', style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text('$address'),
+                    ]
+                ),
+              ),
+              SizedBox(height: 18),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return ChangePasswordModal();
+                        },
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        side:
+                        BorderSide(color: Colors.purple, width: 2.0),
+                      ),
+                      primary: Colors.white,
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      minimumSize: Size(35.0, 25.0),
+                    ),
+                    child: Text(
+                      'Đổi mật khẩu',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      _showLogoutConfirmation(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        side:
+                        BorderSide(color: Colors.purple, width: 2.0),
+                      ),
+                      primary: Colors.white,
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      minimumSize: Size(35.0, 25.0),
+                    ),
+                    child: Text(
+                      'Logout',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
